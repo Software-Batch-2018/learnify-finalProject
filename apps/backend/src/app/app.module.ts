554257b8a73fa4dbfa2f5487c3d1d2621c8da414
Kusal Lamsal <kusal@yarsa.io>
2023-05-18 02:39:18 +0545

    Add docker file for postgres initialization, add typeorm and postgres connection configuration on backend

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: '127.0.0.1',
+      port: 5432,
+      username: 'postgres',
+      password: 'learnifydbpassword',
+      database: 'learnify',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

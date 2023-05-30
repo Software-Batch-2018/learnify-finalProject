024ec9d8dad1edbae1fbfea2ec5c5e9dfae609b2
kusaljr <prastutlamsal41@gmail.com>
2023-05-30 22:47:16 +0545

    Fixed sidebar

--- apps/admin-panel/components/layout.tsx
+++ apps/admin-panel/components/layout.tsx
@@ -4,15 +4,13 @@ import Sidebar from './sidebar';
 
 const Layout = ({ children }: any) => {
   return (
-    <>
-      <div className="flex flex-auto h-screen">
-        <Sidebar />
-        <div className="grow">
-          <Navbar />
-          <div className="m-5">{children}</div>
-        </div>
+    <div className="flex flex-auto h-screen">
+      <Sidebar />
+      <div className="flex flex-col flex-1 overflow-auto">
+        <Navbar />
+        <div className="m-5 flex-1 overflow-auto">{children}</div>
       </div>
-    </>
+    </div>
   );
 };
 

5753247fbb9ce2fdf9ee0f4a5437638ec7cebe28
Dipesh Subedi <dipeshsubedi@Dipeshs-MacBook-Air.local>
2023-06-02 18:36:57 +0545

    Logout functionality

--- apps/app/src/screens/profile.screen.tsx
+++ apps/app/src/screens/profile.screen.tsx
@@ -1,9 +1,47 @@
-import { Box, Text } from 'native-base';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { CommonActions } from '@react-navigation/native';
+import { Box, Button, Text } from 'native-base';
+import React from 'react';
 
-export default function ProfilePage() {
+export default function ProfilePage({ navigation }: any) {
+  const getData = React.useCallback(async () => {
+    try {
+      const value = await AsyncStorage.getItem('token');
+      if (value === null) {
+        navigation.dispatch(
+          CommonActions.reset({
+            index: 0,
+            routes: [{ name: 'Login' }],
+          })
+        );
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  }, [navigation]);
+
+  const clearAll = async () => {
+    try {
+      await AsyncStorage.clear();
+    } catch (e) {
+      // clear error
+    }
+
+    console.log('Done.');
+  };
+  const handleLogout = () => {
+    clearAll().then(() => {
+      navigation.replace('Login');
+    });
+  };
+
+  React.useEffect(() => {
+    getData();
+  }, [getData, navigation]);
   return (
-    <Box>
+    <Box p={2}>
       <Text>Logged In</Text>
+      <Button onPress={handleLogout}>Logout</Button>
     </Box>
   );
 }
